87edd1c5d8a30256fc721ea547347f78a34b987b
Alex Fabre <alexfabre@ymail.com>
2016-04-28 12:30:56 +0200

    comment in progress

--- comment.py
+++ comment.py
@@ -14,6 +14,8 @@ repo=sys.argv[3] # The project to analyse
 fileName='comment'+org+'-'+repo+'.json'
 file=''
 user=''
+max_file_comment=0
+max_user_comment=0
 UnknownUser_id=1 #Will be used to identify a Unknown user
 # Sharing the data in the common 'sh.py' to all modules
 sh.fileName=fileName
@@ -33,22 +35,25 @@ gh = Github(login=u, password=p)
 
 def getUserOnIssue():
     issueList={}
+    userList={}
     issue_list = gh.issues.list_by_repo(user=org, repo=repo,state='open').all()
     for resource in issue_list:
-        issueList[resource.number]={'number':resource.number,'author':resource.user.login, 'commenter':{}, 'comment':0}
+        issueList[resource.number]={'number':resource.number,'author':resource.user.login,'state':resource.state, 'commenter':{esource.user.login:1}, 'comment':0}
         print resource.number
         # print resource.user.login
         data=gh.issues.comments.list(number=resource.number, user=org, repo=repo).all()
         for comment in data:
             # print "    ",comment.user.login
             issueList[resource.number]['comment']+=1
-            issueList[resource.number]['commenter'][comment.user.login]=1
+
+            if issueList[resource.number]['commenter'].has_key(comment.user.login):
+            	issueList[resource.number]['commenter'][comment.user.login]+=1
+            else:
+            	issueList[resource.number]['commenter'][comment.user.login]=1
     return issueList
     
 issueList=getUserOnIssue()
 print issueList
-
-quit()
 
 # Giving a single number for the graph drawing to each node according to python's order of dictionary
 nodesNumber=0
@@ -57,13 +62,30 @@ cpt=0 # To attribute each node a unique number for the links of the graph
 
 print '\033[4m'+"----Making json file: "+fileName+" ----"+'\033[0m'
 
-print "[issue]",
+print "[issues]",
 json = open(fileName, "wb+")
 json.write( '{"nodes":[');
 for issue in issueList:
-    issueList[issue]['num']=cpt
+issueList    if issueList[issue]['comment']>max_file_comment:
+    	max_file_comment=issueList[issue]['commits']
     cpt+=1
-    json.write('{"number":"'+issueList[issue]['number']+'","author":"'+issueList[issue]['author']+'","num":"'+str(issueList[issue]['num'])+'","comments":"'+str(issueList[issue]['comments'])+'","group":1},')
+    json.write('{"number":"'+issueList[issue]['number']+'","state":"'+issueList[issue]['state']+'","num":"'+str(issueList[issue]['num'])+'","comment":"'+str(issueList[issue]['comment'])+'","group":1},')
+print '\033[92m'+" ok"+'\033[0m'
+
+
+print "[commenters]",
+cemaphore=True
+for contributor in contributersList:
+    contributersList[contributor]['num']=cpt
+    if contributersList[contributor]['commits']>max_user_commit:
+    	max_user_commit=contributersList[contributor]['commits']
+    cpt+=1
+    if cemaphore:
+        json.write('{"login":"'+contributersList[contributor]['login']+'","num":"'+str(contributersList[contributor]['num'])+'","commits":"'+str(contributersList[contributor]['commits'])+'","name":"'+contributersList[contributor]['name']+'","type":"user","id":"'+str(contributersList[contributor]['id'])+'","group":'+str((contributersList[contributor]['commits']/20)+4)+'}')
+        cemaphore=False
+    else:
+        json.write(',{"login":"'+contributersList[contributor]['login']+'","num":"'+str(contributersList[contributor]['num'])+'","commits":"'+str(contributersList[contributor]['commits'])+'","name":"'+contributersList[contributor]['name']+'","type":"user","id":"'+str(contributersList[contributor]['id'])+'","group":'+str((contributersList[contributor]['commits']/20)+4)+'}')
+              
 print '\033[92m'+" ok"+'\033[0m'
 
 
@@ -86,4 +108,4 @@ json.close()
 sh.max_user_commit=max_user_commit
 sh.max_file_commit=max_file_commit
 
-import module_drawing
+import module_3Dgraph
