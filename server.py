7d1f6759622c7ddaf40ee63d1e6982c11b7b3e0a
Javier Canovas <me@jlcanovas.es>
2016-05-24 09:24:07 +0200

    Changing endpoint in index.html

--- server.py
+++ server.py
@@ -14,47 +14,46 @@ def index():
 
 @app.route("/log")
 def login():
-	
-	user = request.values.get('user')
-	p = request.values.get('pass')
-	org = request.values.get('org')
-	repo = request.values.get('repo')
-	
-	logged,message,foundRepo = gph.Prepare(user,p,org,repo)
+    user = request.values.get('user')
+    p = request.values.get('pass')
+    org = request.values.get('org')
+    repo = request.values.get('repo')
 
-	rep = {'login_succeded':logged,'message':message,'repo_exists':foundRepo}
+    logged, message, foundRepo = gph.Prepare(user, p, org, repo)
 
-	return Response(json.dumps(rep), mimetype='text/json')
+    rep = {'login_succeded': logged, 'message': message, 'repo_exists': foundRepo}
+
+    return Response(json.dumps(rep), mimetype='text/json')
+
 
 @app.route("/getCont")
 def getContribution():
-	
-	layout1, data1=gph.graphContribution()
+    layout1, data1 = gph.graphContribution()
 
-	rep={'layout':layout1,'data':data1}
+    rep = {'layout': layout1, 'data': data1}
 
-	return Response(json.dumps(rep), mimetype='text/json')
+    return Response(json.dumps(rep), mimetype='text/json')
+
 
 @app.route("/getComm")
 def getComments():
-	
-	layout2, data2=gph.graphComments()
+    layout2, data2 = gph.graphComments()
 
-	rep={'layout':layout2,'data':data2}
+    rep = {'layout': layout2, 'data': data2}
 
-	return Response(json.dumps(rep), mimetype='text/json')
+    return Response(json.dumps(rep), mimetype='text/json')
+
 
 @app.route("/getAll")
 def getAllRepo():
+    rep = gph.graphMetrics()
 
-	
-	rep=gph.graphMetrics()
-	
-	return Response(json.dumps(rep), mimetype='text/json')
+    return Response(json.dumps(rep), mimetype='text/json')
+
 
 if __name__ == "__main__":
-	app.run()
-	url_for('static', filename='styles.css')
-	url_for('static', filename='dessin.svg')
-	url_for('static', filename='error.svg')
-	url_for('static', filename='unknown.gif')
+    app.run()
+    url_for('static', filename='styles.css')
+    url_for('static', filename='dessin.svg')
+    url_for('static', filename='error.svg')
+    url_for('static', filename='unknown.gif')
