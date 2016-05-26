55298fbac1e63bf6a4c062207433522019e47c87
Alex Fabre <alexfabre@ymail.com>
2016-05-26 15:19:15 +0200

    Getting clear

--- metric.py
+++ metric.py
@@ -21,27 +21,30 @@ def contribution(file):
 	for file in data['files']:
 		cemaphore=True
 		file_list={}
+		author=file['author']
 		for commiter in file['commiters']:
 			if commiter_list.has_key(commiter['login']):
 				pass
 			else:
-				commiter_list[commiter['login']]=num
+				commiter_list[commiter['login']]={'num':num,'FA':0,'DL':0,'AC':0}
 				name_list.append(commiter['login'])
 				num+=1
 			if cemaphore:
-				file_list[commiter['login']]=commiter_list[commiter['login']]
+				file_list[commiter['login']]=commiter_list[commiter['login']]['num']
 				cemaphore=False
 			else:
 				for login in file_list:
-					if commiter_list[commiter['login']]<file_list[login]:
-						one=commiter_list[commiter['login']]
+					if commiter_list[commiter['login']]['num']<file_list[login]:
+						one=commiter_list[commiter['login']]['num']
 						two=file_list[login]
 					else:
 						one=file_list[login]
-						two=commiter_list[commiter['login']]
+						two=commiter_list[commiter['login']]['num']
+
+					print one,two
 
 					Edges[(one,two)]={'one':one,'two':two,'size':1}
-				file_list[commiter['login']]=commiter_list[commiter['login']]
+				file_list[commiter['login']]=commiter_list[commiter['login']]['num']
 
 	table=[]
 	for couple in Edges:
@@ -65,15 +68,15 @@ def contribution(file):
 	visual_style["bbox"] = (wi, wi)
 	visual_style["margin"] = (wi/10)
 
-	return Graph.vs.select(_degree = Graph.maxdegree())["name"]
+	Graph.vs.select(_degree = Graph.maxdegree())["name"]
 
 
 
-	visual=False
+	visual=True
 
 	if visual:
-
-		return plot(Graph, **visual_style)
+		
+		plot(Graph, **visual_style)
 	
 
 if __name__ == "__main__":
